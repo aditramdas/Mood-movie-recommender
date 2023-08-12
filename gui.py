e8febb419821204f8be658e8e1516270a45bb71b
Adith Ramdas <aditramdas@gmail.com>
2023-08-12 16:35:33 +0530

    First commit

---
+++ gui.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from tkinter import ttk
+import scraper
+
+MOOD_MAPPING = {
+    'Happy': ['comedy', 'musical', 'feel-good'],
+    'Sad': ['drama', 'tragedy'],
+    'Adventurous': ['action', 'adventure', 'fantasy'],
+    'Relaxed': ['drama', 'romance', 'slice of life']
+} 
+
+
+def fetch_and_recommend():
+    mood = mood_combobox.get()
+    global movies
+    movies = scraper.fetch_movies_from_imdb(MOOD_MAPPING[mood])
+    for movie in movies:
+        title, year = movie
+        recommendations_list.insert(tk.END, f"{title} ({year})")
+
+app = tk.Tk()
+app.title("IMDB Movie Recommender")
+
+frame = ttk.Frame(app, padding="10")
+frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+label = tk.Label(app, text="Select your mood:")
+label.grid(row=0, column=1, sticky=tk.W, pady=0)
+
+mood_combobox = ttk.Combobox(app, values=list(MOOD_MAPPING.keys()))
+mood_combobox.grid(row=0, column=4, pady=0)
+
+recommend_button = tk.Button(app, text="Recommend Movies", command=fetch_and_recommend)
+recommend_button.grid(row=4, column=0, columnspan=2, pady=5)
+
+recommendations_list = tk.Listbox(frame, height=10, width=50)
+recommendations_list.grid(row=5, column=0, columnspan=2, pady=0)
+
+app.mainloop()
+
---
+++ scraper.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_genre_from_movie_page(movie_url, mood):
+    HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+    }
+    response = requests.get(movie_url, headers=HEADERS)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    genres=[]
+    for td in soup.find_all('a', class_='ipc-chip ipc-chip--on-baseAlt'):
+        genres.append(td.span.text)
+    if any(genre.lower() in mood for genre in genres):
+        print(genres)
+        return genres
+
+
+def fetch_movies_from_imdb(mood ): 
+    HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+    }
+    print(mood)
+    url = 'https://www.imdb.com/chart/top'
+    response = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(response.text, 'lxml')
+    movies = []
+    count = 0
+    for td in soup.find_all('li' , class_='ipc-metadata-list-summary-item sc-bca49391-0 eypSaE cli-parent'):
+        title = td.find('h3' , class_='ipc-title__text').text.split(".")[1]
+        year = td.find('span' , class_='sc-14dd939d-6 kHVqMR cli-title-metadata-item').text
+        movie_url = 'https://www.imdb.com' + td.find('a' , class_='ipc-title-link-wrapper')['href']
+        genres = fetch_genre_from_movie_page(movie_url , mood)
+        if genres:
+            movies.append((title, year))
+            count += 1
+        if count == 5:
+            break
+    return movies
+
